cf90176a565e1f41bf6dd1f30bbfa08326cbac4a
Phen Thach <pshannonthach@gmail.com>
2019-09-05 20:58:53 -0400

    js lesson 5

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-// lesson 4
+// lesson 5
 
-var wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
-
-var books = document.querySelector('#book-list li .name');
-console.log(books);
-
-books = document.querySelectorAll('#book-list li .name');
-console.log(books);
+var books = document.querySelectorAll('#book-list li .name');
 
 Array.from(books).forEach(function(book){
-  console.log(book);
+    book.textContent += ' (Book title)';
 });
+
+var bookList = document.querySelector('#book-list');
+bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML content</p>';
