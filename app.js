216a64667a108b3465af5743f1201c9a3f23a60f
Phen Thach <pshannonthach@gmail.com>
2019-09-09 18:09:38 -0400

    lesson 11

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-// lesson 10
+// lesson 11
 
 var list = document.querySelector('#book-list ul');
 
-// delete books
+//delete books
 list.addEventListener('click', (e) => {
-  if(e.target.className == 'delete'){
-    const li = e.target.parentElement;
-    li.parentNode.removeChild(li);
-  }
+    if(e.target.className == 'delete'){
+        var li = e.target.parentElement;
+        li.parentNode.removeChild(li);
+    }
+});
+
+var forms = document.forms;
+console.log(forms);
+console.log(forms['add-book']);
+
+Array.from(forms).forEach(function(form){
+    console.log(form);
+});
+
+var addForm = forms['add-book'];
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    var value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
 });
