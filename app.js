1ec13817cefae4928075b0336c47730fd6ae275f
Phen Thach <pshannonthach@gmail.com>
2019-09-09 18:00:51 -0400

    lesson 10

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-// lesson 9
+// lesson 10
 
-var listItems = document.querySelectorAll('#book-list ul li');
+var list = document.querySelector('#book-list ul');
 
-Array.from(listItems).forEach(function(item){
-  item.addEventListener('click', (e) => {
-
-    var li = e.target.parentElement;
-    console.log('child element to remove:', li);
-    console.log('parent element to remove child from:', li.parentElement);
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
     li.parentNode.removeChild(li);
-
-  });
-});
-
-// prevent default behaviour
-
-var link = document.querySelector('#page-banner a');
-
-link.addEventListener('click', function(e){
-  e.preventDefault();
-  console.log('Navigation to', e.target.textContent, 'was prevented');
+  }
 });
